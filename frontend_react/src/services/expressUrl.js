725376561ed3b0269f4390b10e935d29a0bfd760
Siegfried <siegfried.bozza@renuo.ch>
2023-03-02 20:36:46 +0100

    add process.env.NODE_ENV to assign expressUrls

--- frontend_react/src/services/expressUrl.js
+++ frontend_react/src/services/expressUrl.js
@@ -1,6 +1,8 @@
-const EXPRESS_URL_NOTES = '/api/notes'
-const EXPRESS_URL_LOGIN = '/api/login'
-// const EXPRESS_URL_NOTES = 'http://localhost:3001/api/notes'
-// const EXPRESS_URL_LOGIN = 'http://localhost:3001/api/login'
+const EXPRESS_URL_NOTES = process.env.NODE_ENV === 'development' ?
+    'http://localhost:3001/api/notes'
+    : '/api/notes'
+const EXPRESS_URL_LOGIN = process.env.NODE_ENV === 'development' ?
+    'http://localhost:3001/api/login'
+    : '/api/login'
 
 export { EXPRESS_URL_NOTES, EXPRESS_URL_LOGIN }
