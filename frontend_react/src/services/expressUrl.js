6201bbfe369e2ea4d08c0d09a6cc7e732f16598b
Siegfried <siegfried.bozza@renuo.ch>
2023-03-02 17:15:33 +0100

    fix express server urls

--- frontend_react/src/services/expressUrl.js
+++ frontend_react/src/services/expressUrl.js
@@ -1,5 +1,6 @@
-// export const EXPRESS_URL = '/api/notes'
-const EXPRESS_URL_NOTES = 'http://localhost:3001/api/notes'
-const EXPRESS_URL_LOGIN = 'http://localhost:3001/api/login'
+const EXPRESS_URL_NOTES = '/api/notes'
+const EXPRESS_URL_LOGIN = '/api/login'
+// const EXPRESS_URL_NOTES = 'http://localhost:3001/api/notes'
+// const EXPRESS_URL_LOGIN = 'http://localhost:3001/api/login'
 
 export { EXPRESS_URL_NOTES, EXPRESS_URL_LOGIN }
